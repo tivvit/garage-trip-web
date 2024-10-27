e6350f8068e6bbd9f230cba97ad0a89829918bb8
Vít Listík <tivvit@seznam.cz>
2024-10-27 23:26:45 +0100

    add images, minification pipelne and modals

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
-const { src, dest } = require('gulp');
+import gulp from 'gulp';
+import webp from 'gulp-webp';
+import { src, dest, parallel, series } from 'gulp';
+import imageResize from 'gulp-image-resize';
 
-exports.default = function() {
-  return src('node_modules/bootstrap/scss/**')
-    .pipe(dest('assets/css/boostrap'));
+function bootstrap(cb) {
+  src('node_modules/bootstrap/scss/**')
+  .pipe(dest('assets/css/boostrap'));
+  cb();
 }
+
+function images(cb) {
+  [400, 2000].forEach(function (size) {
+    src('_images/*.{jpg,jpeg,png}', {encoding: false})
+      .pipe(imageResize({ width: size }))
+      .pipe(webp())
+      .pipe(dest(`assets/images/${size}/`))
+  });
+  cb();
+}
+
+export default parallel(images, bootstrap);
